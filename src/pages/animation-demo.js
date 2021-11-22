dd04babbfd73e0fcdbc5c5cac8afd49d9f5f173b
anujsolvtive <anuj@solvative.com>
2021-11-22 14:02:11 +0530

    animation

--- src/pages/animation-demo.js
+++ src/pages/animation-demo.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useRef, useState } from 'react';
 import Layout from '../components/layout'
-import * as LottiePlayer from "@lottiefiles/lottie-player";
+import { Player, Controls } from '@lottiefiles/react-lottie-player';
 
 const onPlayClickHandler = (ref,setIsPlaying) => {
     ref.current.play();
@@ -12,36 +12,38 @@ const onPauseClickHandler = (ref,setIsPlaying) => {
     setIsPlaying(false);
 }
 
-const onGoClickHandler = (ref,seekValue) => {
-    ref.current.seek(seekValue+"%");
+const onGoClickHandler = (ref,lottie,seekValue) => {
+    ref.current.setSeeker(seekValue);
+    //lottie.goToAndStop(seekValue+"%", false);
 }
 
-const onPlayListener = (e) => {
+const onPlayListener = (event) => {
     console.log("animation play started");
 }
 
 const AnimationDemo = () => {
-    const ref = useRef();
+    const playerRef = useRef();
     const [isPlaying, setIsPlaying] = useState(true);
     const [seekValue, setSeekValue] = useState("");
-    useEffect(() => {
-        console.log("mounted");
-        ref.current.addEventListener('play',onPlayListener)
-    },[])
+    const [lottie, setLottieInstance] = useState({});
     return (
         <Layout pageTitle="Animation Demo">
             <p>Checkout animation</p>
-            <lottie-player
+            <Player
+                onEvent={event => {
+                    if (event === 'play') onPlayListener(event); // check event type and do something
+                  }}
                 autoplay
                 loop
-                ref={ref}
+                ref={playerRef}
+                lottieRef={instance => setLottieInstance(instance)}
                 mode="normal"
                 src="https://assets9.lottiefiles.com/private_files/lf30_kd6vmszk.json"
                 style={{width: "320px"}}
-            ></lottie-player>
-            {(isPlaying) ? <button onClick={() => onPauseClickHandler(ref,setIsPlaying)}>Pause</button> : <button onClick={() => onPlayClickHandler(ref,setIsPlaying)}>Play</button> }
+            ></Player>
+            {(isPlaying) ? <button onClick={() => onPauseClickHandler(playerRef,setIsPlaying)}>Pause</button> : <button onClick={() => onPlayClickHandler(playerRef,setIsPlaying)}>Play</button> }
             Go to timeline: <input type="number" min="0" max="100" value={seekValue} onChange={e => setSeekValue(e.target.value) }/>
-            <button onClick={() => onGoClickHandler(ref,seekValue)}>Go</button>
+            <button onClick={() => onGoClickHandler(playerRef,lottie,seekValue)}>Go</button>
         </Layout>
     )
 }
